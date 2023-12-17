d37e13aebc4e0c98b714ca742ee9f91e005955e3
BIPLOB <developerbiplob.bd@gmail.com>
2023-12-17 22:08:11 +0600

    React dom using NavLink

--- React-basic/src/component/Header.jsx
+++ React-basic/src/component/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
 
     return (
         <div>
             <ul style={{ display:"flex", justifyContent:"center", gap:"2em", listStyle:'none', backgroundColor:"#ddd", padding:"20px" }}>
-                <li><Link to="/">Home</Link></li>
+                {/* <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
-                <li><Link to="/service">Service</Link></li>
+                <li><Link to="/service">Service</Link></li> */}
+                <NavLink className={({isActive})=>isActive? "text-success":"text-primary"} to="/">Home</NavLink>
+                <NavLink className={({isActive})=>isActive? "text-success":"text-primary"} to="/about">About</NavLink>
+                <NavLink className={({isActive})=>isActive? "text-success":"text-primary"} to="/service">Service</NavLink>
             </ul>
         </div>
     );
